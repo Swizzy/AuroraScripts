555cddc43173569485e4ad803e7d0d8e01638c05
Swizzy <swizzy@xeupd.com>
2014-09-07 23:08:29 +0200

    Fixed Hide Backups so that it works
    Signed-off-by: Swizzy <swizzy@xeupd.com>

--- Media/Scripts/UserFilters/HideBackups.lua
+++ Media/Scripts/UserFilters/HideBackups.lua
@@ -1,4 +1,4 @@
 GameListFilterCategories.User["Hide Backups"] = function(Content)
 	-- Return if this game ends with .bak
-	return not string.sub(Content.Executable, -4) == ".bak"
+	return not (string.sub(Content.Executable, -4) == ".bak")
 end
