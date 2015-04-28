11348e5f5bfda61725dfc0577231db7d47826c20
Swizzy <Swizzy@users.noreply.github.com>
2015-04-28 22:06:44 +0200

    Update HideBackups.lua

--- Media/Scripts/UserFilters/HideBackups.lua
+++ Media/Scripts/UserFilters/HideBackups.lua
@@ -1,6 +1,6 @@
 GameListFilterCategories.User["Hide Backups"] = function(Content)
 	-- Matches NXE2GOD backups
-	if ((string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\00007000\\backup") ~= nil))
+	if (string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\00007000\\backup") ~= nil)
 		return false; -- Don't show this item
 	-- Return if this game ends with .bak
 	return not (string.lower(string.sub(Content.Executable, -4)) == ".bak")
