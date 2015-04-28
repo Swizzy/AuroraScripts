317f34ad279b31aef229d0c8b4231fca7f41be43
Swizzy <Swizzy@users.noreply.github.com>
2015-04-28 22:06:10 +0200

    Update HideBackups.lua
    Added support for NXE2GOD backups

--- Media/Scripts/UserFilters/HideBackups.lua
+++ Media/Scripts/UserFilters/HideBackups.lua
@@ -1,4 +1,7 @@
 GameListFilterCategories.User["Hide Backups"] = function(Content)
+	-- Matches NXE2GOD backups
+	if ((string.match(Content.Directory, "0000000000000000\\[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\00007000\\backup") ~= nil))
+		return false; -- Don't show this item
 	-- Return if this game ends with .bak
 	return not (string.lower(string.sub(Content.Executable, -4)) == ".bak")
 end
