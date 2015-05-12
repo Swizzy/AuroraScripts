693c22f05e2580a5be6268d5cc9abc1f3b47c2e2
Swizzy <Swizzy@users.noreply.github.com>
2015-05-12 15:52:10 +0200

    Update LastPlayed.lua
    Hopefully fixed errors now

--- Media/Scripts/UserSorts/LastPlayed.lua
+++ Media/Scripts/UserSorts/LastPlayed.lua
@@ -1,12 +1,12 @@
 GameListSorters["Last Played"] = function(Item1, Item2, Type)
-	if Type == SortType.Descending then
-		if ((Item1.LastPlayed.HighPart == 0 and Item1.LastPlayed.LowPart == 0) or (Item2.LastPlayed.HighPart == 0 and Item2.LastPlayed.LowPart == 0) or (Item1.LastPlayed == Item2.LastPlayed)) then
+	if (Type == SortType.Descending) then
+		if ((Item1.LastPlayed.HighPart == 0) and (Item1.LastPlayed.LowPart == 0) or (Item2.LastPlayed.HighPart) == 0 and (Item2.LastPlayed.LowPart == 0) or (Item1.LastPlayed == Item2.LastPlayed)) then
 			return string.lower(Item1.Name) > string.lower(Item2.Name)
 		end
-		return (Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart or Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart)
+		return ((Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart) or (Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart))
 	end
-	if ((Item1.LastPlayed.HighPart == 0 and Item1.LastPlayed.LowPart == 0) or (Item2.LastPlayed.HighPart == 0 and Item2.LastPlayed.LowPart == 0) or (Item1.LastPlayed == Item2.LastPlayed)) then
+	if ((Item1.LastPlayed.HighPart == 0) and (Item1.LastPlayed.LowPart == 0) or (Item2.LastPlayed.HighPart == 0) and (Item2.LastPlayed.LowPart == 0) or (Item1.LastPlayed == Item2.LastPlayed)) then
 		return string.lower(Item1.Name) < string.lower(Item2.Name)
 	end
-	return (Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart or Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart)
+	return ((Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart) or (Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart))
 end
