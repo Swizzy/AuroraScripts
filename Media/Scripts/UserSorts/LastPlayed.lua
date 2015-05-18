27f2b229c8acc77544cc31c5d15caa6dff375538
Swizzy <swizzy@xeupd.com>
2015-05-18 15:16:57 +0200

    Fixed LastPlayed sorting script to work correctly

--- Media/Scripts/UserSorts/LastPlayed.lua
+++ Media/Scripts/UserSorts/LastPlayed.lua
@@ -1,12 +1,27 @@
 GameListSorters["Last Played"] = function(Item1, Item2, Type)
 	if (Type == SortType.Descending) then
-		if ((Item1.LastPlayed.HighPart == 0) and (Item1.LastPlayed.LowPart == 0) or (Item2.LastPlayed.HighPart == 0) and (Item2.LastPlayed.LowPart == 0) or (Item1.LastPlayed == Item2.LastPlayed)) then
-			return string.lower(Item1.Name) > string.lower(Item2.Name)
+		if ((Item1.LastPlayed.HighPart == 0) and (Item2.LastPlayed.HighPart == 0)) then
+			return (string.lower(Item1.Name) > string.lower(Item2.Name))
+		elseif ((Item1.LastPlayed.HighPart ~= 0) and (Item2.LastPlayed.HighPart == 0)) then
+			return false
+		elseif ((Item1.LastPlayed.HighPart == 0) and (Item2.LastPlayed.HighPart ~= 0)) then
+			return true
+		elseif (Item1.LastPlayed.HighPart == Item2.LastPlayed.HighPart) then
+			return (Item1.LastPlayed.HighPart < Item2.LastPlayed.HighPart)
+		else
+			return (Item1.LastPlayed.LowPart < Item2.LastPlayed.LowPart)
 		end
-		return ((Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart) or (Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart))
+	else
+		if ((Item1.LastPlayed.HighPart == 0) and (Item2.LastPlayed.HighPart == 0)) then
+			return string.lower(Item1.Name) < string.lower(Item2.Name)
+		elseif ((Item1.LastPlayed.HighPart ~= 0) and (Item2.LastPlayed.HighPart == 0)) then
+			return true
+		elseif ((Item1.LastPlayed.HighPart == 0) and (Item2.LastPlayed.HighPart ~= 0)) then
+			return false
+		elseif (Item1.LastPlayed.HighPart == Item2.LastPlayed.HighPart) then
+			return (Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart)
+		else
+			return (Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart)
+		end
 	end
-	if ((Item1.LastPlayed.HighPart == 0) and (Item1.LastPlayed.LowPart == 0) or (Item2.LastPlayed.HighPart == 0) and (Item2.LastPlayed.LowPart == 0) or (Item1.LastPlayed == Item2.LastPlayed)) then
-		return string.lower(Item1.Name) < string.lower(Item2.Name)
-	end
-	return ((Item1.LastPlayed.HighPart > Item2.LastPlayed.HighPart) or (Item1.LastPlayed.LowPart > Item2.LastPlayed.LowPart))
 end
